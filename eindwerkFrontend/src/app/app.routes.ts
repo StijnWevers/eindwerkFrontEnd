739c4db1c8acb8a31b2ca2ce8c43275033c3b73a
StijnWevers <stijn.wevers@hotmail.be>
2025-01-10 10:47:58 +0100

    Angular opgezet

---
+++ eindwerkFrontend/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MessagesComponent } from './pages/messages/messages.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'messages', component: MessagesComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'settings', component: SettingsComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+    
+})
+export class AppRoutingModule { }
+
+
