0b81be3787fb658cded38c4f253ecb7ba75b89de
Yuriy Tsayukov <yuriy.tsayukov@outlook.com>
2025-01-26 18:27:14 +0100

    refactor: Update comments for clarity in language settings

--- eindwerkFrontend/src/app/pages/settings/settings.component.ts
+++ eindwerkFrontend/src/app/pages/settings/settings.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './settings.component.css',
 })
 export class SettingsComponent {
-  // Welke taal is actief
+  //actieve taal
   currentLanguage: string = 'Nederlands';
 
   // Hier komen je vertalingen
@@ -15,18 +15,17 @@ export class SettingsComponent {
     Nederlands: {
       settingsTitle: 'Instellingen',
       languageSectionTitle: 'Taal wijzigen',
-      languageAlertText: (lang: string) => `Taal gewijzigd naar: ${lang}`,
-      // Enzovoort, elk stukje tekst dat je wilt tonen
+      // languageAlertText: (lang: string) => `Taal gewijzigd naar: ${lang}`,
     },
     Engels: {
       settingsTitle: 'Settings',
       languageSectionTitle: 'Change Language',
-      languageAlertText: (lang: string) => `Language changed to: ${lang}`,
+      // languageAlertText: (lang: string) => `Language changed to: ${lang}`,
     },
     Chinees: {
       settingsTitle: '设置',
       languageSectionTitle: '更改语言',
-      languageAlertText: (lang: string) => `语言更改为: ${lang}`,
+      // languageAlertText: (lang: string) => `语言更改为: ${lang}`,
     },
   };
 
