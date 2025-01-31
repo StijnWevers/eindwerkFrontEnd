879a3b8dc24e45c60577a793570a4084dfa8481d
Sennedwt <sennedewit16@icloud.com>
2025-01-31 16:16:55 +0100

    Profile verbeterd

--- eindwerkFrontend/src/app/pages/profile/profile.component.ts
+++ eindwerkFrontend/src/app/pages/profile/profile.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { AuthService } from '../../services/auth.service'; 
 import { DatabaseService } from '../../services/database.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
